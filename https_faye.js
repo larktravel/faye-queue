7369328405f32e23f94a9c1a3ed0589e01e50c6d
Koushik Dasika <koushikdasika@gmail.com>
2015-09-16 11:23:44 -0400

    blah

--- https_faye.js
+++ https_faye.js
@@ -1,6 +1,7 @@
 require ("daemon")();
 
-var https = require('https'),
+//var https = require('https'),
+var https = require('http'),
     redis = require('faye-redis'),
     fs    = require('fs'),
     faye  = require('faye');
@@ -12,6 +13,7 @@ var bayeux = new faye.NodeAdapter({
     type: redis
 }});
 
+/*
 var options = {
   key: fs.readFileSync("./key.pem"),
   cert: fs.readFileSync("./cert.pem"),
@@ -22,6 +24,13 @@ var server = https.createServer(options, function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Hello, non-Bayeux request\n');
 });
+*/
+
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello, non-Bayeux request\n');
+});
+
 
 bayeux.attach(server);
 server.listen(5223);
